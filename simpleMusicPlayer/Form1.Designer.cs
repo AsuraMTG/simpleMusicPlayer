9f3de36ac58736fd8209aeec2606c3673882caff
Masznyik Márton <eper.karma@gmail.com>
2023-10-21 16:44:07 +0200

    3 Album and List 3 working songs

--- simpleMusicPlayer/Form1.Designer.cs
+++ simpleMusicPlayer/Form1.Designer.cs
@@ -30,18 +30,167 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(383, 388);
+            this.pictureBox1.Location = new System.Drawing.Point(375, 375);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(50, 50);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "1.\t\"Green Room\"",
+            "2.\t\"Jennifer\'s Body\"",
+            "3.\t\"Fighting My Demons\"",
+            "4.\t\"Singapore\"",
+            "5.\t\"Lose It\"",
+            "6.\t\"Hardcore\"",
+            "7.\t\"Me N My Kup\"",
+            "8.\t\"It\'s Over\"",
+            "9.\t\"Succubus\"",
+            "10.\t\"Paranoid\"",
+            "11.\t\"Pots\"",
+            "12.\t\"Like This\"",
+            "13.\t\"Overtime\"",
+            "14.\t\"Vampire Hour\"",
+            "15.\t\"Nightcore\"",
+            "16.\t\"Nightcore 2\"",
+            "17.\t\"Rockstar Lifestyle\"",
+            "18.\t\"I Need U\""});
+            this.listBox1.Location = new System.Drawing.Point(624, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(164, 238);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(381, 359);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(10);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click_1);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(12, 252);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(10);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(12, 132);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(10);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "1.\t\"How U Feel?\"",
+            "2.\t\"If Looks Could Kill\"",
+            "3.\t\"Fly Sht\"",
+            "4.\t\"Which One\"",
+            "5.\t\"Raver\"",
+            "6.\t\"Came in Wit\'\"",
+            "7.\t\"By the Pound\"",
+            "8.\t\"All the Time\"",
+            "9.\t\"Biggest Problem\"",
+            "10.\t\"Chris Paul\"",
+            "11.\t\"Superstar\"",
+            "12.\t\"New New\"",
+            "13.\t\"Right Now\"",
+            "14.\t\"Which Way\"",
+            "15.\t\"Wagwan\"",
+            "16.\t\"Moment of Silence\"\t",
+            "17.\t\"Brazy Girls\"",
+            "18.\t\"Goin\' Up\"",
+            "19.\t\"Passenger\"",
+            "20.\t\"Promo\"",
+            "21.\t\"Worth It\"\t",
+            "22.\t\"Redlight\"\t",
+            "23.\t\"Make Sum Work\"",
+            "24.\t\"Safety (Interlude)\"",
+            "25.\t\"Your Eyes\""});
+            this.listBox2.Location = new System.Drawing.Point(624, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(164, 329);
+            this.listBox2.TabIndex = 7;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Items.AddRange(new object[] {
+            "1.\t\"Rockstar Made\"",
+            "2.\t\"Go2DaMoon\"",
+            "3.\t\"Stop Breathing\"",
+            "4.\t\"Beno!\"",
+            "5.\t\"JumpOutTheHouse\"",
+            "6.\t\"M3tamorphosis\"",
+            "7.\t\"Slay3r\"",
+            "8.\t\"No Sl33p\"",
+            "9.\t\"New Tank\"",
+            "10.\t\"Teen X\"",
+            "11.\t\"Meh\"",
+            "12.\t\"Vamp Anthem\"",
+            "13.\t\"New N3on\"",
+            "14.\t\"Control\"",
+            "15.\t\"Punk Monk\"",
+            "16.\t\"On That Time\"",
+            "17.\t\"King Vamp\"",
+            "18.\t\"Place\"",
+            "19.\t\"Sky\"",
+            "20.\t\"Over\"",
+            "21.\t\"ILoveUIHateU\"",
+            "22.\t\"Die4Guy\"",
+            "23.\t\"Not PLaying\"",
+            "24.\t\"F33l Lik3 Dyin\""});
+            this.listBox3.Location = new System.Drawing.Point(624, 12);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(164, 316);
+            this.listBox3.TabIndex = 8;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
             // 
             // Form1
             // 
@@ -49,7 +198,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Menu;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Location = new System.Drawing.Point(960, 540);
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(816, 489);
@@ -58,14 +215,25 @@
             this.Name = "Form1";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Simple Music Player";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
     }
 }
 
--- simpleMusicPlayer/Form1.cs
+++ simpleMusicPlayer/Form1.cs
@@ -4,17 +4,38 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.Reflection;
+using System.IO;
+using System.Resources;
 
 namespace simpleMusicPlayer
 {
+    
     public partial class Form1 : Form
     {
+        public static void PlayMusic(string filepath, bool playorstop)
+        {
+            SoundPlayer musicPlayer = new SoundPlayer();
+            if (playorstop == true)
+            {
+                musicPlayer.SoundLocation = filepath;
+                musicPlayer.Play();
+            }
+            else
+                musicPlayer.Stop();
+        }
         public Form1()
         {
             InitializeComponent();
+            listBox1.Hide();
+            listBox2.Hide();
+            listBox3.Hide();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -31,12 +52,100 @@ namespace simpleMusicPlayer
         {
 
             pictureBox1.Image = System.Drawing.Image.FromFile(@"C:\Users\Asura\Desktop\Programing\simpleMusicPlayer\simpleMusicPlayer\bin\Debug\play.png");
-            
+            PlayMusic("Vamp.wav", false);
         }
 
         private void pictureBox2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var Album1 = new List<string>()
+            {
+                "1.\t\"Green Room\"",
+                "2.\t\"Jennifer\'s Body\"",
+                "3.\t\"Fighting My Demons\"",
+                "4.\t\"Singapore\"",
+                "5.\t\"Lose It\"",
+                "6.\t\"Hardcore\"",
+                "7.\t\"Me N My Kup\"",
+                "8.\t\"It's Over\"",
+                "9.\t\"Succubus\"",
+                "10.\t\"Paranoid\"",
+                "11.\t\"Pots\"",
+                "12.\t\"Like This\"",
+                "13.\t\"Overtime\"",
+                "14.\t\"Vampire Hour\"",
+                "15.\t\"Nightcore\"",
+                "16.\t\"Nightcore 2\"",
+                "17.\t\"Rockstar Lifestyle\"",
+                "18.\t\"I Need U\""
+            };
+            var Album1tsongs = new List<string>()
+            {
+                "GreenRoom.wav",
+                "Jennifer'sBody.wav",
+                "FightingMyDemons.wav",
+                "4.\t\"Singapore\"",
+                "5.\t\"Lose It\"",
+                "6.\t\"Hardcore\"",
+                "7.\t\"Me N My Kup\"",
+                "8.\t\"It's Over\"",
+                "9.\t\"Succubus\"",
+                "10.\t\"Paranoid\"",
+                "11.\t\"Pots\"",
+                "12.\t\"Like This\"",
+                "13.\t\"Overtime\"",
+                "14.\t\"Vampire Hour\"",
+                "15.\t\"Nightcore\"",
+                "16.\t\"Nightcore 2\"",
+                "17.\t\"Rockstar Lifestyle\"",
+                "18.\t\"I Need U\""
+            };
+
+            for (int i = 0; i < listBox1.Items.Count; i++) 
+            {
+                if (listBox1.Text == Album1[i])
+                {
+                    pictureBox1.Image = System.Drawing.Image.FromFile(@"C:\Users\Asura\Desktop\Programing\simpleMusicPlayer\simpleMusicPlayer\bin\Debug\pause.png");
+                    label1.Text = Album1[i];
+                    PlayMusic(Album1tsongs[i], true);
+                }
+            }
+            
+        }
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click_1(object sender, EventArgs e)
+        {
+            listBox1.Show();
+            listBox2.Hide();
+            listBox3.Hide();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            listBox1.Hide();
+            listBox2.Show();
+            listBox3.Hide();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            listBox1.Hide();
+            listBox2.Hide();
+            listBox3.Show();
         }
     }
 }
